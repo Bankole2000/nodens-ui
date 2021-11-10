4c72f27fd647f6188041f071133fe255403b7952
Bankole Esan <techybanky@gmail.com>
2021-11-10 13:47:07 +0100

    minor edits

--- src/components/blocks/LeadBlock.vue
+++ src/components/blocks/LeadBlock.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="lead-block">
-    <v-img :aspect-ratio="1" class="rounded-xl mr-4" :src="require(`@/assets/imgs/${lead.image}.jpg`)" />
+    <v-img :aspect-ratio="1" class="rounded-xl mr-4" :src="require(`@/assets/imgs/${lead.image}.jpg`)">
+    <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular
+          size="70"
+          width="5"
+            indeterminate
+            color="secondary lighten-1"
+          ></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
     <p class="grey--text title mb-1 mt-4 pl-2">{{lead.title}}</p>
     <p class="headline pl-2">{{lead.name}}</p>
   </div>
